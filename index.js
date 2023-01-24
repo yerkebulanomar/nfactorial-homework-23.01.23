4236174db5fcc0fd3800d013628e3e708a1b40e7
yerkebulan.omar <yerkebulan.omar@gmail.com>
2023-01-25 01:09:40 +0600

    hw done

---
+++ index.js
@@ -0,0 +1,30 @@
+// Task 1
+
+// Встроенная функция setTimeout использует колбэк-функции. Создайте альтернативу, использующую промисы.
+
+// Функция delay(ms) должна возвращать промис, который перейдёт в состояние «выполнен» через ms миллисекунд,
+// так чтобы мы могли добавить к нему .then:
+
+// function delay(ms) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("выполнилось через 3 секунды"), ms);
+//   });
+// }
+
+// delay(3000).then((message) => alert(message));
+
+// Task 2
+
+let promise = new Promise(function (resolve, reject) {
+  resolve(1);
+
+  setTimeout(() => resolve(2), 1000); // will be ignored
+});
+
+promise.then(alert);
+
+// Task 3
+
+promise.then(f1).catch(f2); // shows resolve return if promise complited, if not => reject return complited
+
+promise.then(f1, f2); // shows only resolve returns
